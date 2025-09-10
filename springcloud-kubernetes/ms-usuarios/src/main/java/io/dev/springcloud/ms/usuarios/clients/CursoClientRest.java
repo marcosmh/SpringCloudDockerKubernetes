b7515988fd92794149a14fd75172d710e0b068c4
Marcos Magaña <imarcosmagana@gmail.com>
2025-09-09 19:27:55 -0600

    modificacion api rest

--- springcloud-kubernetes/ms-usuarios/src/main/java/io/dev/springcloud/ms/usuarios/clients/CursoClientRest.java
+++ springcloud-kubernetes/ms-usuarios/src/main/java/io/dev/springcloud/ms/usuarios/clients/CursoClientRest.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-cursos", url = "localhost:8002")
+@FeignClient(name = "ms-cursos", url = "host.docker.internal:8002")
 public interface CursoClientRest {
 
 
